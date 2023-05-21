003e0e542716d30409ccedbd3297e1e0fca04c8e
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-05-21 16:32:16 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# CUDA Robert's Filter
+
+Robert's Filter implementation in CUDA.
+
+Assingment for Graphics Card Programming course at Technical University of Lodz.
---
+++ main.cu
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "lib/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "lib/stb_image_write.h"
+
+#define CHANNELS 3
+#define BLOCK_SIZE 32
+
+int main()
+{
+    std::cout << "Works!\n";
+}
